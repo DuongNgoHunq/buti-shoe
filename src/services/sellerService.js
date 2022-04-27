174b6317a4f1ca78597d29fff5a9859825f1486a
DuongNgoHunq <duongngohung1118@gmail.com>
2022-04-27 16:55:33 +0700

    Lay lai userService.js (Tu nhien bi mat)

--- src/services/sellerService.js
+++ src/services/sellerService.js
@@ -1,16 +1,22 @@
 import db from "../models/index";
 
 
-let getTopSellerHome = (limit) => {
+let getTopSellerHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
         try {
             let sellers = await db.User.findAll({
-                limit: limit,
+                limit: limitInput,
                 where: { roleId: 'R2' },
                 order: [['createdAt', 'DESC']],
                 attributes: {
-                    exclude: ['password', 'image']
-                }
+                    exclude: ['password']
+                },
+                include: [
+                    // { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                    { model: db.Allcode, as: 'genderData', attributes: ['valueEn', 'valueVi'] }
+                ],
+                raw: true,
+                nest: true
             })
             resolve({
                 errCode: 0,
